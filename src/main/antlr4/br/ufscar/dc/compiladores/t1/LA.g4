2057d031787106099dde3a66b36aa5da7d58c11c
Guilherme Calça <guilhermecalca@hotmail.com>
2023-05-23 15:34:29 -0300

    first commit

---
+++ src/main/antlr4/br/ufscar/dc/compiladores/t1/LA.g4
@@ -0,0 +1,5 @@
+lexer grammar LA;
+
+Algoritmo: 'algoritmo';
+Fim_algoritmo: 'fim_algoritmo';
+Declare: 'declare';
---
+++ src/main/java/br/ufscar/dc/compiladores/t1/App.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.compiladores.t1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
